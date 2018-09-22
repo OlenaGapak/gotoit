ad098116f95377acc61b18aa3b4d90c5343994a8
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-09-22 12:10:45 +0300

    epoch config created

---
+++ src/game/knowledge/epoch.js
@@ -0,0 +1,27 @@
+export const epoch = {
+    epoch_1: {
+        name: "Epoch 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur, culpa cumque dignissimos iure necessitatibus neque odit temporibus ut voluptates",
+        start_tick: 0
+    },
+    epoch_2: {
+        name: "Epoch 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur, culpa cumque dignissimos iure necessitatibus neque odit temporibus ut voluptates",
+        start_tick: 0
+    },
+    epoch_3: {
+        name: "Epoch 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur, culpa cumque dignissimos iure necessitatibus neque odit temporibus ut voluptates",
+        start_tick: 0
+    },
+    epoch_4: {
+        name: "Epoch 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur, culpa cumque dignissimos iure necessitatibus neque odit temporibus ut voluptates",
+        start_tick: 0
+    },
+    epoch_5: {
+        name: "Epoch 5",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus consequatur, culpa cumque dignissimos iure necessitatibus neque odit temporibus ut voluptates",
+        start_tick: 0
+    }
+};
